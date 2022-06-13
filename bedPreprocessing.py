0b48298587fa408d0254f5de2d9e4430643bdbee
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2022-06-13 17:06:21 -0500

    Create bedPreprocessing.py
    convert gata3 bed file to a input file compatible with any numerical ML method

---
+++ bedPreprocessing.py
@@ -0,0 +1,27 @@
+import csv
+
+positive_path = "GATA3_positive_examples.csv"
+negative_path = "GATA3_negative_examples.csv"
+
+fasta = open("gata3_negative.fa", "w")
+fasta.write("sequence")
+fasta.write("\t")
+fasta.write("label")
+fasta.write("\n")
+
+with open(negative_path, 'r') as f:          # Read lines separately
+    reader = csv.reader(f, delimiter=',')
+    for line in enumerate(reader):
+        data=str(line[1]).upper()
+        data=data.replace(",", "")
+        data = data.replace("1", "")
+        data = data.replace("'", "")
+        data=data.replace("[", "")
+        data=data.replace("]", "")
+        data = data.replace(" ", "")
+        fasta.write(data)
+        fasta.write("\t")
+        fasta.write("0")
+        fasta.write("\n")
+f.close()
+fasta.close()
