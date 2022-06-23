c165df736c17b30ef2ae02376a54aff03e8dde21
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2022-06-23 14:16:49 -0500

    Update kmers_XGBoost.py
    accuracy = 0.717 
    precision = 0.728 
    recall = 0.717 
    f1 = 0.713

--- kmers_XGBoost.py
+++ kmers_XGBoost.py
@@ -1,26 +1,29 @@
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from xgboost import XGBClassifier
 import pandas as pd
-import matplotlib.pyplot as plt
+
 
 # function to convert sequence strings into k-mer words, default size = 6 (hexamer words)
 def getKmers(sequence, size=6):
     return [sequence[x:x+size].lower() for x in range(len(sequence) - size + 1)]
 
-
+#read preprocessed table, see github repository
 gata3_data=pd.read_table("final")
-print(gata3_data)
+#print(gata3_data)
 
+##replace seuence column with kmers words
 gata3_data['words'] = gata3_data.apply(lambda x: getKmers(x['sequence']), axis=1)
 gata3_data = gata3_data.drop('sequence', axis=1)
-#print(gata3_data)
+print(gata3_data)
 
 human_texts = list(gata3_data['words'])
 for item in range(len(human_texts)):
     human_texts[item] = ' '.join(human_texts[item])
+
 y_data = gata3_data.iloc[:, 0].values
 
-#print(human_texts[26451])
-#print(y_data)
+print(human_texts[0])
+print(human_texts[1])
 
 # Creating the Bag of Words model using CountVectorizer()
 # This is equivalent to k-mer counting
@@ -29,37 +32,37 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(ngram_range=(4,4))
 X = cv.fit_transform(human_texts)
 
-#print(X.shape)
+print(X)
 gata3_data['label'].value_counts().sort_index().plot.bar()
 
 seed = 7
-test_size = 0.33
+test_size = 0.3
 # Splitting the human dataset into the training set and test set
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, RepeatedStratifiedKFold, cross_val_score
+
 X_train, X_test, y_train, y_test = train_test_split(X,
                                                     y_data,
                                                     test_size=test_size,
                                                     random_state=seed)
 
+# instantiate the classifier
+xgb_clf = XGBClassifier()
 
-# fit model no training data
-model = XGBClassifier()
-model.fit(X_train, y_train)
+# fit the classifier to the training data
+xgb_clf.fit(X_train, y_train)
 
-#print(model)
-# make predictions for test data
-y_pred = model.predict(X_test)
-predictions = [round(value) for value in y_pred]
+y_pred = xgb_clf.predict(X_test)
+#print('XGBoost model accuracy score: {0:0.4f}'. format(accuracy_score(y_test, y_pred)))
 
-
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 print("Confusion matrix\n")
 print(pd.crosstab(pd.Series(y_test, name='Actual'), pd.Series(y_pred, name='Predicted')))
+
 def get_metrics(y_test, y_predicted):
     accuracy = accuracy_score(y_test, y_predicted)
     precision = precision_score(y_test, y_predicted, average='weighted')
     recall = recall_score(y_test, y_predicted, average='weighted')
     f1 = f1_score(y_test, y_predicted, average='weighted')
     return accuracy, precision, recall, f1
+
 accuracy, precision, recall, f1 = get_metrics(y_test, y_pred)
 print("accuracy = %.3f \nprecision = %.3f \nrecall = %.3f \nf1 = %.3f" % (accuracy, precision, recall, f1))
