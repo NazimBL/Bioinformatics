e72cc4eb6dd2f2582ec70f16c33b80c7621784fd
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2022-06-23 19:49:10 -0500

    Update xgboost_param_optimisation.py

--- xgboost_param_optimisation.py
+++ xgboost_param_optimisation.py
@@ -2,8 +2,16 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from xgboost import XGBClassifier
 import pandas as pd
 import matplotlib.pyplot as plt
-from numpy import mean
+from numpy import mean, nan
 from numpy import std
+
+## Correlation
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# manual nested cross-validation for random forest on a classification dataset
+from sklearn.model_selection import KFold
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 
 # import packages for hyperparameters tuning
 from hyperopt import STATUS_OK, Trials, fmin, hp, tpe
@@ -13,23 +21,21 @@ def getKmers(sequence, size=6):
     return [sequence[x:x+size].lower() for x in range(len(sequence) - size + 1)]
 
 #read preprocessed table, see github repository
-gata3_data=pd.read_table("final")
-print(gata3_data)
+gata3_data=pd.read_table("data")
+#print(gata3_data)
 
 ##replace seuence column with kmers words
 gata3_data['words'] = gata3_data.apply(lambda x: getKmers(x['sequence']), axis=1)
 gata3_data = gata3_data.drop('sequence', axis=1)
-print(gata3_data)
+#print(gata3_data)
 
 human_texts = list(gata3_data['words'])
 for item in range(len(human_texts)):
     human_texts[item] = ' '.join(human_texts[item])
 
 y_data = gata3_data.iloc[:, 0].values
-
-
-print(human_texts[26451])
-#print(y_data)
+#print(human_texts[0])
+#print(human_texts[1])
 
 # Creating the Bag of Words model using CountVectorizer()
 # This is equivalent to k-mer counting
@@ -38,7 +44,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 cv = CountVectorizer(ngram_range=(4,4))
 X = cv.fit_transform(human_texts)
 
-print(X.shape)
+#print(X)
 gata3_data['label'].value_counts().sort_index().plot.bar()
 
 seed = 7
@@ -51,44 +57,54 @@ X_train, X_test, y_train, y_test = train_test_split(X,
                                                     test_size=test_size,
                                                     random_state=seed)
 
-space={'max_depth': hp.quniform("max_depth", 3, 18, 1),
-        'gamma': hp.uniform ('gamma', 1,9),
-        'reg_alpha' : hp.quniform('reg_alpha', 40,180,1),
-        'reg_lambda' : hp.uniform('reg_lambda', 0,1),
-        'colsample_bytree' : hp.uniform('colsample_bytree', 0.5,1),
-        'min_child_weight' : hp.quniform('min_child_weight', 0, 10, 1),
-        'n_estimators': 180,
-        'seed': 0
-    }
+## Hyper Parameter Optimization
+params = {
+    "learning_rate": [0.05, 0.10, 0.15, 0.20, 0.25, 0.30],
+    "max_depth": [3, 4, 5, 6, 8, 10, 12, 15],
+    "min_child_weight": [1, 3, 5, 7],
+    "gamma": [0.0, 0.1, 0.2, 0.3, 0.4],
+    "colsample_bytree": [0.3, 0.4, 0.5, 0.7]
 
+}
 
-def objective(space):
-    model = XGBClassifier(
-        n_estimators=space['n_estimators'], max_depth=int(space['max_depth']), gamma=space['gamma'],
-        reg_alpha=int(space['reg_alpha']), min_child_weight=int(space['min_child_weight']),
-        colsample_bytree=int(space['colsample_bytree']))
+best_params={'min_child_weight': 7, 'max_depth': 10, 'learning_rate': 0.25, 'gamma': 0.0, 'colsample_bytree': 0.7}
 
-    evaluation = [(X_train, y_train), (X_test, y_test)]
+from datetime import datetime
+def timer(start_time=None):
+    if not start_time:
+        start_time = datetime.now()
+        return start_time
+    elif start_time:
+        thour, temp_sec = divmod((datetime.now() - start_time).total_seconds(), 3600)
+        tmin, tsec = divmod(temp_sec, 60)
+        print('\n Time taken: %i hours %i minutes and %s seconds.' % (thour, tmin, round(tsec, 2)))
 
-    model.fit(X_train, y_train,
-            eval_set=evaluation, eval_metric="auc",
-            early_stopping_rounds=10, verbose=False)
+# instantiate the classifier
+classifier = XGBClassifier()
 
-    pred = model.predict(X_test)
-    accuracy = accuracy_score(y_test, pred > 0.5)
-    print("SCORE:", accuracy)
-    return {'loss': -accuracy, 'status': STATUS_OK}
+random_search=RandomizedSearchCV(classifier,param_distributions=params,n_iter=5,scoring='roc_auc',n_jobs=-1,cv=5,verbose=3)
+# Here we go
+start_time = timer(None) # timing starts from this point for "start_time" variable
+random_search.fit(X,y_data)
+timer(start_time) # timing ends here for "start_time" variable
 
+print(random_search.best_estimator_)
+print(random_search.best_params_)
 
+#Input best_estimator output to classifier's param
 
-trials = Trials()
+classifier=XGBClassifier(base_score=0.5, booster='gbtree', callbacks=None,
+              colsample_bylevel=1, colsample_bynode=1, colsample_bytree=0.7,
+              early_stopping_rounds=None, enable_categorical=False,
+              eval_metric=None, gamma=0.0, gpu_id=-1, grow_policy='depthwise',
+              importance_type=None, interaction_constraints='',
+              learning_rate=0.25, max_bin=256, max_cat_to_onehot=4,
+              max_delta_step=0, max_depth=10, max_leaves=0, min_child_weight=7,
+              missing=None, monotone_constraints='()', n_estimators=100,
+              n_jobs=0, num_parallel_tree=1, predictor='auto', random_state=0,
+              reg_alpha=0, reg_lambda=1, scale_pos_weight=1, seed=None,subsample=1)
 
-best_hyperparams = fmin(fn = objective,
-                        space = space,
-                        algo = tpe.suggest,
-                        max_evals = 100,
-                        trials = trials)
+from sklearn.model_selection import cross_val_score
+score=cross_val_score(classifier,X,y_data,cv=10)
 
-
-print("The best hyperparameters are : ","\n")
-print(best_hyperparams)
+print(score)
