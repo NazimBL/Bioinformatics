5a34f94dd17ec60aa66e1e6fe009f70de8aae223
Belabbaci A.Nazim <30374106+NazimBL@users.noreply.github.com>
2022-07-26 15:03:52 -0500

    Update coverage_mean.py

--- coverage_mean.py
+++ coverage_mean.py
@@ -1,43 +1,41 @@
 import os
-
 import pandas as pd
 
-positive_path = "f_healthy_coverage_1.tab"
+for filename in os.listdir("new/"):
 
+    fasta = open(filename+"_out.tab", "w")
 
-fasta = open("f_healthy_coverage_1_out.tab", "w")
+    with open("new/"+filename, 'r') as f:          # Read lines separately
+        Lines = f.readlines()
+        for line in Lines:
 
-with open(positive_path, 'r') as f:          # Read lines separately
-    Lines = f.readlines()
-    for line in Lines:
+            from re import search
+            substring = "chrY"
 
-        from re import search
-        substring = "chrY"
-
-        if search(substring, str(line)): fasta.write(str(line))
-        else: continue
+            if search(substring, str(line)): fasta.write(str(line))
+            else: continue
         
-f.close()
-fasta.close()
+    f.close()
+    fasta.close()
 
 #loop this
-df=pd.read_table("f_healthy_coverage_1_out.tab")
+    df=pd.read_table(filename+"_out.tab")
     # Drop single column by Index
-print(df.head())
-df2=df.drop(df.columns[[0, 1, 2]], axis = 1)
-df2.to_csv('hout.csv', index=False)
+    print(df.head())
+    df2=df.drop(df.columns[[0, 1, 2]], axis = 1)
+    df2.to_csv(filename+"_out.csv", index=False)
 
-given_file = open('hout.csv', 'r')
+    given_file = open(filename+"_out.csv", 'r')
 
-lines = given_file.readlines()
-sum = 0
+    lines = given_file.readlines()
+    sum = 0
 
-for line in lines:
-    for c in line:
-        if c.isdigit() == True:
-            sum = sum + int(c)
+    for line in lines:
+        for c in line:
+            if c.isdigit() == True:
+                sum = sum + int(c)
 
-print(sum)
+    print(sum)
 
-given_file.close()
-os.rename('hout.csv','hout_'+str(sum)+'.csv')
+    given_file.close()
+    os.rename(filename+"_out.csv",filename+str(sum)+'.csv')
